99f71a7809d144832e3c3b9fced04bfbe3938f77
Ivan <mirage101@abv.bg>
2021-02-23 23:08:49 +0200

    Upload files to git

---
+++ src/firebase/config.js
@@ -0,0 +1,14 @@
+var firebaseConfig = {
+  apiKey: "AIzaSyAMTMnKBO-CiWQou8bT6PE0atIKfno_xjc",
+  authDomain: "hook-news123.firebaseapp.com",
+  databaseURL: "https://hook-news123.firebaseio.com",
+  projectId: "hook-news123",
+  storageBucket: "hook-news123.appspot.com",
+  messagingSenderId: "751964137248",
+  appId: "1:751964137248:web:5aed7e937ce615cf275945",
+  measurementId: "G-KH88NF0LYM"
+};
+// Initialize Firebase
+
+
+export default firebaseConfig;
